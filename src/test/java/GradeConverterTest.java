66a6aaefa8e62fddf0f3294a714d46eae35a17d3
patri27826 <patrick41513@gmail.com>
2022-11-08 11:20:55 +0800

    Test Code

---
+++ src/test/java/GradeConverterTest.java
@@ -0,0 +1,27 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class GradeConverterTest {
+    private GradeConverter converter;
+
+    @Before
+    public void setUp() throws Exception {
+        converter = new GradeConverter();
+    }
+
+    @Test
+    public void testConvert1() {
+        String expected = "A";
+        String actual = converter.Convert(100);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testConvert2() {
+        String expected = "A";
+        String actual = converter.Convert(90);
+        assertEquals(expected, actual);
+    }
+}
